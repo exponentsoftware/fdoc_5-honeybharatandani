2c3a40324fab9a358644b8818c8c68999077a1fc
honeybharatandani <andanihoney106@gmail.com>
2021-10-03 20:14:46 +0530

    updates

--- index.js
+++ index.js
@@ -2,14 +2,35 @@
 
 const API_URL = "https://restcountries.com/v3/all";
 
-// const data = async () => {
-//   const api = await fetch(API_URL).then(response => response.json());
-//   console.log(api);
-// };
-// data();
-
 const fetchData = async () => {
-  const api = await fetch(API_URL).then(response => response.json());
-  console.log(api);
+  const countries = await fetch(API_URL).then(response => response.json());
+  for (let i = 0; i <= countries.length; i++) {
+    console.log([countries[i].name, countries[i].languages]);
+  }
 };
 fetchData();
+
+// fetch("https://restcountries.com/v3/all")
+//   .then(data => {
+//     return data.json();
+//     // console.log(data.json());
+//   })
+
+//   .then(countries => {
+//     let lang = [];
+//     for (let i = 0; i < countries.length; i++) {
+
+//       if (countries[i].languages) {
+
+//       }}
+
+//     console.log(countries);
+//     })
+//   // console.log(countries[i]);
+//   // for (let i = 0; i < countries.length; i++) {
+//   //   console.log(actualdata[i]);
+//   // }
+
+//   .catch(error => {
+//     console.log(error);
+//   });
